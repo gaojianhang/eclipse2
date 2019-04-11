6485b2a8067d25715c641fe3b63b92752d764bc0
gaojianhang <13363230218@163.com>
2019-04-11 14:09:31 +0800

    ytrty

--- hello2/src/com/gao/H.java
+++ hello2/src/com/gao/H.java
@@ -3,15 +3,7 @@ package com.gao;
 public class H {
 	//这里是分支
 	public static void main(String[] args) {
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
-		System.out.println("你好git");
+		System.out.println("这里是分支");
 	}
 
 }
